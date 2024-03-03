55fecff19d855de3895438501272e87a1bbbee1a
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-03-04 00:00:11 +0530

    color toggler

--- 19.color-toggler-app/build/js/app.js
+++ 19.color-toggler-app/build/js/app.js
@@ -1,1 +1,9 @@
 console.log("hello");
+let arr = [1, 2, 3, 4, 5];
+function findAndSet(arr, num) {
+  let elem = arr.find((item) => item === num);
+  elem = 0;
+}
+
+findAndSet(arr, 3);
+console.log(arr);
