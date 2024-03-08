c8a0f8d6bea7caa773b5aeea9bbf29d0f868b9d2
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-03-08 23:52:47 +0530

    making dark theme toggler

--- 19.color-toggler-app/build/js/app.js
+++ 19.color-toggler-app/build/js/app.js
@@ -1,9 +1,6 @@
-console.log("hello");
-let arr = [1, 2, 3, 4, 5];
-function findAndSet(arr, num) {
-  let elem = arr.find((item) => item === num);
-  elem = 0;
-}
-
-findAndSet(arr, 3);
-console.log(arr);
+const toggler = document.getElementById("toggler");
+toggler.addEventListener("click", () => {
+  toggler.checked === true
+    ? (document.body.style.backgroundColor = "black")
+    : (document.body.style.backgroundColor = "white");
+});
